505f5679d084bf0ca7c595f0e32637957651c923
Brianojunga <brianojunga3@gmail.com>
2025-11-01 20:19:49 +0300

    Implement different kinds of join

---
+++ database-adv-script/README.md
@@ -0,0 +1,2 @@
+This queries higlight a small implementation of joins for quering.
+The joins include Full outer join, left join and inner joins 
---
+++ database-adv-script/joins_queries.sql
@@ -0,0 +1,40 @@
+SELECT 
+    Booking.Property_id,
+    Booking.guest_id,
+    Users.full_name,
+    Users.email
+FROM 
+    Booking
+INNER JOIN
+    Users
+ON 
+    Booking.guest_id = Users.user_id;
+
+
+
+SELECT
+    Property.property_id,
+    Property.price_per_night,
+    Property.max_guest,
+    Review.comment,
+    Review.rating
+FROM
+    Property
+LEFT JOIN 
+    Review
+ON 
+    Property.property_id = Review.property_id
+
+SELECT
+    SELECT 
+    Booking.Property_id,
+    Booking.guest_id,
+    Users.full_name,
+    Users.email
+FROM 
+    Users
+FULL OUTER JOIN
+    Booking
+ON 
+    Booking.guest_id = Users.user_id;
+
