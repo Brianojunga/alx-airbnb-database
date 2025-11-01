d47a44244714be4fd1f761bd941601ec12d0b673
Brianojunga <brianojunga3@gmail.com>
2025-11-01 21:53:49 +0300

    Add aggreagtions and window functions

--- database-adv-script/README.md
+++ database-adv-script/README.md
@@ -1,4 +1,6 @@
 This queries higlight a small implementation of joins for quering.
 The joins include Full outer join, left join and inner joins in subqueries
 
-The secons in subqueries implies the use of sub queries to find different aspects of other tables more so for aggregations
+The second in subqueries implies the use of sub queries to find different aspects of other tables more so for aggregations
+
+The aggregations sql file uses the window functions and group by for aggregations
---
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -0,0 +1,15 @@
+-- Write a query to find the total number of bookings made by each user, using the COUNT function and GROUP BY clause.
+
+-- Use a window function (ROW_NUMBER, RANK) to rank properties based on the total number of bookings they have received.
+
+SELECT guest_id, COUNT(booking_id) AS total_bookings FROM Booking
+GROUP BY guest_id;
+
+SELECT 
+    property_id,
+    COUNT(booking_id) AS total_bookings,
+    RANK() OVER (
+        ORDER BY COUNT(booking_id) DESC AS rank
+    )
+FROM Booking
+GROUP BY property_id;
